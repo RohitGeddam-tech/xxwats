bb09cb3ec87c538dfa8295e5d238ba002e8d71a7
RohitGeddam-tech <geddamrohi13@gmail.com>
2021-03-11 21:40:12 +0530

    newest commit

--- src/about/MidDesktop.js
+++ src/about/MidDesktop.js
@@ -57,12 +57,12 @@ const MidDesktop = () => {
                                 <h4 className='flexbox-title'>cyril <img onClick={()=>{setCyril(true)}} src={icon} alt='logo' /></h4>
                             </div>
                             <Fade className={cyrilbox} in={Cyril} delay='500' duration='2000'>
-                                    <div className='fadebox'>
-                                        <p>
-                                        Cyril's spent the past 21 years establishing his credentials in Management, Creative & Production. He has created branded content for Radio, Television & Digital while working with the likes of the TOI’s Entertainment unit, Red FM, Channel V and more. His last stint with O4 Digital - Group M, led to him breaking barriers on the digital platform. He has now blended his content and production expertise with technology, allowing him to be the creative firepower of 20 watts.
-                                        </p>
-                                        <h4 className='flexbox-title'>Anushree <img onClick={()=>{setCyril(false)}} src={icona} alt='logo' /></h4>
-                                    </div>
+                                <div className='fadebox'>
+                                    <p>
+                                    Cyril's spent the past 21 years establishing his credentials in Management, Creative & Production. He has created branded content for Radio, Television & Digital while working with the likes of the TOI’s Entertainment unit, Red FM, Channel V and more. His last stint with O4 Digital - Group M, led to him breaking barriers on the digital platform. He has now blended his content and production expertise with technology, allowing him to be the creative firepower of 20 watts.
+                                    </p>
+                                    <h4 className='flexinbox-title'>Cyril <img onClick={()=>{setCyril(false)}} src={icona} alt='logo' /></h4>
+                                </div>
                             </Fade>
                         </div>
                         <div className='inflex2 animate__animated animate__fadeInLeft animate__delay-3s'>
@@ -75,7 +75,7 @@ const MidDesktop = () => {
                                         <p>
                                         Savio, an expert at his craft but a tech geek at heart with over 20 years of experience in the media industry on the technical as well as editorial front, starting with IN Mumbai News Channel in 2001. He has worked with reputed organisations like Sahara Networks and Network 18. With his ventures Beanstalk Productions and Mad Hatter Media Pvt. Ltd., he has consistently delivered quality services to all major television networks and digital platforms. 
                                         </p>
-                                        <h4 className='flexbox-title'>Anushree <img onClick={()=>{setSavio(false)}} src={icona} alt='logo' /></h4>
+                                        <h4 className='flexinbox-title'>Savio <img onClick={()=>{setSavio(false)}} src={icona} alt='logo' /></h4>
                                     </div>
                             </Fade>
                         </div>
@@ -96,7 +96,7 @@ const MidDesktop = () => {
                                         <p>
                                         She Started with Channel [v], attended New York Film Academy to study Producing for Film & TV and then pulled a ‘Swades’ on everybody. She returned to her motherland to create cool content for digital mediums. If there was an award for ‘Shining Personalities’, Arathi would surely be the runner up. But if we are really being honest about her, she is a creative powerhouse and nothing short of a goofy rockstar!
                                         </p>
-                                        <h4 className='flexbox-title'>Anushree <img onClick={()=>{setArathi(false)}} src={icona} alt='logo' /></h4>
+                                        <h4 className='flexinbox-title'>Arathi <img onClick={()=>{setArathi(false)}} src={icona} alt='logo' /></h4>
                                     </div>
                             </Fade>
                         </div>
@@ -110,7 +110,7 @@ const MidDesktop = () => {
                                         <p>
                                         Savio, an expert at his craft but a tech geek at heart with over 20 years of experience in the media industry on the technical as well as editorial front, starting with IN Mumbai News Channel in 2001. He has worked with reputed organisations like Sahara Networks and Network 18. With his ventures Beanstalk Productions and Mad Hatter Media Pvt. Ltd., he has consistently delivered quality services to all major television networks and digital platforms. 
                                         </p>
-                                        <h4 className='flexbox-title'>Anushree <img onClick={()=>{setAnsh(false)}} src={icona} alt='logo' /></h4>
+                                        <h4 className='flexinbox-title'>Ansh <img onClick={()=>{setAnsh(false)}} src={icona} alt='logo' /></h4>
                                     </div>
                             </Fade>
                         </div>
@@ -131,7 +131,7 @@ const MidDesktop = () => {
                                         <p>
                                         Latest addition to the 20 watts family, she handles recruitments and business management and expansions. She comes with 12+ years of experience in advertising and production since she started working when she was 17 and claims she hasn’t taken a sick day ever since. She has worked with agencies like Ogilvy, Contract, Alok Nanda and Company, Havas Creative, TBWA and more.  She also experiments with curly hair products, has the loudest laugh in the room and loves to travel every chance she gets.
                                         </p>
-                                        <h4 className='flexbox-title'>Anushree <img onClick={()=>{setActive(false)}} src={icona} alt='logo' /></h4>
+                                        <h4 className='flexinbox-title'>Anushree <img onClick={()=>{setActive(false)}} src={icona} alt='logo' /></h4>
                                     </div>
                             </Fade>
                         </div>
@@ -145,7 +145,7 @@ const MidDesktop = () => {
                                         <p>
                                         A veteran at 20 watts, Saloni Shah is a client's dream. Every request is a yes and every 2am call answered, that's how she paves her way into a client's heart. Her sweetness is her strength and her weakness, and we don't say this because she's a Gujrati. She can be found in the nearest Starbucks conquering the world in her 6inch heels. Also, if you know her, you've definitely asked her for the bomb thepla recipe.
                                         </p>
-                                        <h4 className='flexbox-title'>Saloni <img onClick={()=>{setSaloni(false)}} src={icona} alt='logo' /></h4>
+                                        <h4 className='flexinbox-title'>Saloni <img onClick={()=>{setSaloni(false)}} src={icona} alt='logo' /></h4>
                                     </div>
                             </Fade>
                         </div>
