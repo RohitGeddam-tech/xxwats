b350b2edc22f0a7540d41d4b19c8879adb0a918f
RohitGeddam-tech <geddamrohi13@gmail.com>
2021-04-18 12:19:58 +0530

    helmet changes

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./App.css";
 import Hire from "./home/Hire";
 import Nav from "./components/TestNavbar";
+import { Helmet } from "react-helmet";
+import HeadTag from "./HeadTag";
 
 const View = () => {
   return (
     <>
+      <HeadTag
+        title="Contact Us- 20 Watts | Creative Solutions Agency"
+        desc="We are a Creative Solutions Agency backed by an in-house production and post-production team in Mumbai. Contact us to build custom experiences for your brand"
+      />
       <Nav />
-      <section style={{ paddingTop: "65px", background:'#EEEA64' }}>
+      <section style={{ paddingTop: "65px", background: "#EEEA64" }}>
         <Hire />
       </section>
     </>
